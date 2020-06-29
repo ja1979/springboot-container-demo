3a4b5192c11bc8f5d8928d1164e7c766ded93c34
Jorge Aguilar Bravo <jorgeaguilar@softwareevolutivo.com.ec>
2020-06-28 19:35:16 -0500

    Fix checkstyle

--- src/main/java/ec/sevolutivo/demo/DemoApplication.java
+++ src/main/java/ec/sevolutivo/demo/DemoApplication.java
@@ -3,9 +3,9 @@ package ec.sevolutivo.demo;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
 @RestController
